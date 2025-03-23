01619c1efa5791879d4d23efcf63a3e29c635cbc
chiscookeke11 <chiscookeke11@gmail.com>
2025-03-23 12:51:55 +0100

    format code

--- client/app/components/Hero-cards/IceBreaker.tsx
+++ client/app/components/Hero-cards/IceBreaker.tsx
@@ -22,26 +22,14 @@ export default function IceBreaker() {
         },
 
     ]
-
-
-
-
     return (
-        <div className="absolute left-0 top-[-3px] w-[130px] h-[137px] md:w-[180px] md:h-[206px]   lg:w-[390px] lg:h-[412px] bg-[linear-gradient(126.3deg,_#FFFFFF_62%,_rgba(85,_82,_255,_0.76)_127.91%)]  flex items-center  justify-between flex-col rounded-[18px] transform rotate-[-3deg] text-[#000000] overflow-hidden animate-rotate-in-left  " >
+        <div className="absolute left-0 top-0 w-[130px] h-[137px] md:w-[180px] md:h-[206px]   lg:w-[390px] lg:h-[412px] bg-[linear-gradient(126.3deg,_#FFFFFF_62%,_rgba(85,_82,_255,_0.76)_127.91%)]  flex items-center  justify-between flex-col rounded-[18px] transform rotate-[-3deg] text-[#000000] overflow-hidden animate-rotate-in-left  " >
             <div className=" flex flex-col items-start justify-start gap-3 md:gap-1 lg:gap-4 m-3 lg:ml-5 lg:mt-5 " >
 
                 <h2 className="font-bold text-xs md:text-sm lg:text-[30px] lg:leading-[30px] " >Icebreaker Games</h2>
                 <p className="font-medium text-[11px] md:text-xs lg:text-base " >Start meetings with fun! Quick prompts spark connections</p>
             </div>
-
-
-
-
-
             <ul className="hidden md:flex w-full  flex-col items-center justify-center ">
-
-
-
                 {
                     labels.map((label, index) =>
                         <li key={index} className="bg-[#5552FF] px-3 py-2 lg:py-6 lg:px-4 opacity-[0.7]  w-full flex items-center justify-start gap-4 lg:text-base text-[10px] lg:font-bold  "
@@ -55,9 +43,6 @@ export default function IceBreaker() {
                     )
                 }
             </ul>
-
-
-
         </div>
     )
 }
--- client/app/components/Hero.tsx
+++ client/app/components/Hero.tsx
@@ -11,29 +11,16 @@ import TriviaBattles from "./Hero-cards/TriviaBattles";
 export default function Hero() {
     return (
         <section className=" font-grotesk w-full min-h-screen  flex flex-col items-center justify-center gap-10 p-[5%] relative " >
-
-
-
-
             <div className=" flex flex-col items-center justify-center gap-4 text-center  z-10 " >
                 <h1 className="max-w-[676px] text-[#FFFFFF] font-bold text-4xl md:text-[57px] md:leading-[64px] " >Next-level gaming, powered by web3</h1>
                 <p className="font-medium text-sm md:text-2xl " >Promoting team bonding for teams, one game at a time</p>
                 <button className="p-4 h-[56px] w-[200px] cursor-pointer bg-[linear-gradient(255.22deg,#5552FF_-0.14%,#AA2C82_62.58%,#DA100B_81.98%)] rounded-[12px] text-[#ffffff] text-xl font-bold hover:scale-105 transition duration-200 ease-in-out  " >Create team</button>
             </div>
-
-
-
             <div className="lg:w-[100%]   md:w-[60%] w-[98%] max-h-[425.36px] z-10 relative flex items-center justify-center  " >
-
-
                 <IceBreaker />
                 <RankAndReward />
                 <TriviaBattles />
             </div>
-
-
-
-
             <div className="w-full h-[47.125vh] absolute left-0 bottom-0 z-0 " >
                 <Image src={"/background/abrstract-dots.svg"} alt="image" height={100} width={100} className="w-full h-full object-cover " />
             </div>
