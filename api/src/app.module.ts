9dae8b7600833b1a01f673f88dec92f064ebe27b
Azeezat Omobolanle Nasir <omobolanlehazeezat@gmail.com>
2025-03-24 10:41:19 +0100

    Feat: add local DB creation script and integrate with submissions service

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { SubmissionsModule } from './submissions/submissions.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, SubmissionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
