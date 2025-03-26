f69bc36712a2747777461241279e4bad527329d7
mayurlad <mayursanjaylad@gmail.com>
2025-03-26 14:31:12 +0530

    Fixed navbar responsiveness for mobile

--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Navbar = () => {
     return (
-        <div className="border-b border-[#2C2C2C] flex justify-between items-center py-4 lg:py-6 px-4 lg:px-18 ">
+        <div className="border-b border-[#2C2C2C] flex flex-wrap justify-between items-center py-3 px-4 lg:py-5 lg:px-10">
             <div className="flex items-center space-x-3">
                 <Image
                     src="/logo.png"
@@ -11,11 +11,11 @@ const Navbar = () => {
                     height={15}
                     className="block"
                 />
-                <h1 className="text-[20px] md:text-[24px] font-bold">
+                <h1 className="text-[18px] md:text-[22px] lg:text-[24px] font-bold">
                     TEAMVERSE
                 </h1>
             </div>
-            <div>
+            <div className="mt-3 lg:mt-0">
                 <ConnectWallet />
             </div>
         </div>
@@ -24,10 +24,8 @@ const Navbar = () => {
 
 const ConnectWallet = () => {
     return (
-        <button className="bg-[#1C1924] py-2 px-4 rounded-xl lg:text-[16px]">
-            <span className="gradientText lg:text-[16px] text-[14px]">
-                Connect to Wallet
-            </span>
+        <button className="bg-[#1C1924] py-2 px-4 rounded-xl text-[14px] md:text-[16px]">
+            <span className="gradientText">Connect to Wallet</span>
         </button>
     );
 };
