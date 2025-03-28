a8f905cde181a39be8cb860f420ea9c858bd68ae
Aboogeeky <Idrishassan35@gmail.com>
2025-03-28 10:39:27 +0100

    Update and rename index.html to index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import Navbar from "./components/Navbar";
+
+export default function Home() {
+  return (
+    <div className="bg-black min-h-screen">
+      <Navbar />
+      {/* Other page content here */}
+    </div>
+  );
+}
