c2df12a2283c65730c8a1ac8bbe1006d6665b2a4
Qendresa Hoti <qendresahhoti@gmail.com>
2025-03-31 12:54:22 +0200

    Resolved GitHub PR conflict by syncing files with dev

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './modules/prisma/prisma.module';
-import { InviteModule } from './modules/invite/invite.module';
+
 @Module({
-  imports: [PrismaModule, InviteModule],
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
