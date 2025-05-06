6571f3798b0d3c4c8d6eaa95702c31f42ec7638a
kuku67011 <hyzeala28@gmail.com>
2025-05-06 16:50:32 +0100

    corrected the conflict

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
-// app/components/ClientLayout.tsx
 "use client";
 import { Geist, Geist_Mono, Space_Grotesk } from 'next/font/google';
-<<<<<<< HEAD
 import React from 'react';
 import { DarkModeProvider } from '../components/DarkModeContext';
-=======
 import './styles/tailwind.css';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
->>>>>>> 2f39bb008f34401168ab9a819f9c98058fbd8565
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
@@ -25,39 +21,30 @@ const spaceGrotesk = Space_Grotesk({
   subsets: ['latin'],
 });
 
-
 interface LayoutProps {
   children: React.ReactNode;
 }
 
-
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <html lang="en">
-<<<<<<< HEAD
-      <body>
-      <DarkModeProvider>
-        <div
-          className="flex flex-col min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white transition-colors duration-300 "
-          style={{
-            backgroundImage: "url('/image.svg')", 
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        >
-          {children}
-        </div>
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} ${spaceGrotesk.variable} antialiased`}>
+      <body style={{ fontFamily: 'var(--font-space-grotesk)' }}>
+        <DarkModeProvider>
+          <div
+            className="flex flex-col min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white transition-colors duration-300"
+            style={{
+              backgroundImage: "url('/image.svg')",
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+          >
+            {children}
+          </div>
         </DarkModeProvider>
-=======
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${spaceGrotesk.variable} antialiased`}
-        style={{ fontFamily: 'var(--font-space-grotesk)' }}
-      >
-        {children}
         <Toaster />
->>>>>>> 2f39bb008f34401168ab9a819f9c98058fbd8565
       </body>
     </html>
   );
 };
+
 export default Layout;
